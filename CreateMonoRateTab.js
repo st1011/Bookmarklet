ee1d13e84bd4ce575fae787d71cb60eb88fb36a5
Shohei Tochiyama <s.tochiyama@hotmail.co.jp>
2020-03-14 20:12:29 +0900

    Amazonの製品番号取得処理が違ったので修正

--- CreateMonoRateTab.js
+++ CreateMonoRateTab.js
@@ -3,7 +3,7 @@
      * Amazonのサイトから該当するモノレートのページへジャンプする
      */
     const res = document.URL.match(/https?:\/\/.*amazon\..*\/(product|dp)\/([a-zA-Z0-9]+)\/.*/);
-    if (!res || res.length === 3) {
+    if (!res || res.length !== 3) {
         alert('Amazonの製品URLを見つけられませんでした');
         return;
     }
